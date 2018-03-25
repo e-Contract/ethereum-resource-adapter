0d49a0b53bd19e01c8705c71ae91dbc3987287e2
Frank Cornelis <info@e-contract.be>
2018-03-25 15:57:10 +0200

    XA stuff

--- ethereum-ra/src/main/java/be/e_contract/ethereum/ra/EthereumXAResource.java
+++ ethereum-ra/src/main/java/be/e_contract/ethereum/ra/EthereumXAResource.java
@@ -15,6 +15,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.web3j.protocol.Web3j;
 
+/**
+ * This class still needs some work.
+ *
+ * @author Frank Cornelis
+ */
 public class EthereumXAResource implements XAResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EthereumXAResource.class);
@@ -40,7 +45,7 @@ public class EthereumXAResource implements XAResource {
                 web3j.ethSendRawTransaction(rawTransaction);
             } catch (Exception ex) {
                 LOGGER.error("web3j error: " + ex.getMessage(), ex);
-                throw new XAException();
+                throw new XAException(XAException.XA_HEURRB);
             }
         }
         this.rawTransactions.clear();
@@ -48,7 +53,23 @@ public class EthereumXAResource implements XAResource {
 
     @Override
     public void end(Xid xid, int flags) throws XAException {
-        LOGGER.debug("end: {} - flags {}", xid, false);
+        LOGGER.debug("end: {} - flags {}", xid, flags);
+        if (isFlagSet(flags, TMFAIL)) {
+            this.rawTransactions.clear();
+        }
+        if (!this.rawTransactions.isEmpty()) {
+            try {
+                Web3j web3j = this.ethereumManagedConnection.getWeb3j();
+                // make sure the node is available
+                web3j.ethProtocolVersion().send();
+            } catch (Exception e) {
+                throw new XAException(XAException.XA_RBROLLBACK);
+            }
+        }
+    }
+
+    private boolean isFlagSet(int flags, int mask) {
+        return mask == (flags & mask);
     }
 
     @Override
@@ -65,13 +86,13 @@ public class EthereumXAResource implements XAResource {
     @Override
     public boolean isSameRM(XAResource xaRes) throws XAException {
         LOGGER.debug("isSameRM: {}", xaRes);
-        return false;
+        return this == xaRes;
     }
 
     @Override
     public int prepare(Xid xid) throws XAException {
         LOGGER.debug("prepare: {}", xid);
-        return 0;
+        return XA_OK;
     }
 
     @Override
@@ -90,6 +111,9 @@ public class EthereumXAResource implements XAResource {
     @Override
     public boolean setTransactionTimeout(int seconds) throws XAException {
         LOGGER.debug("setTransactionTimeout: {}", seconds);
+        if (seconds < 0) {
+            throw new XAException();
+        }
         this.transactionTimeout = seconds;
         return true;
     }
@@ -97,7 +121,9 @@ public class EthereumXAResource implements XAResource {
     @Override
     public void start(Xid xid, int flags) throws XAException {
         LOGGER.debug("start: {} - flags {}", xid, flags);
-        this.rawTransactions.clear();
+        if (!isFlagSet(flags, TMRESUME)) {
+            this.rawTransactions.clear();
+        }
     }
 
     void scheduleRawTransaction(String rawTransaction) {
