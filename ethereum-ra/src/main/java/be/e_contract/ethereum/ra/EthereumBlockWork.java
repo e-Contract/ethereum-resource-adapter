6c5e7b439123643a02c3a7871d67640f5609da50
Frank Cornelis <info@e-contract.be>
2019-02-20 14:43:19 +0100

    initial websocket support

--- ethereum-ra/src/main/java/be/e_contract/ethereum/ra/EthereumBlockWork.java
+++ ethereum-ra/src/main/java/be/e_contract/ethereum/ra/EthereumBlockWork.java
@@ -18,15 +18,29 @@
 package be.e_contract.ethereum.ra;
 
 import be.e_contract.ethereum.ra.api.EthereumMessageListener;
+import io.reactivex.Flowable;
+import io.reactivex.disposables.Disposable;
 import java.lang.reflect.Method;
 import java.math.BigInteger;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.resource.spi.endpoint.MessageEndpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.Request;
 import org.web3j.protocol.core.methods.response.EthLog;
+import org.web3j.protocol.core.methods.response.EthSubscribe;
+import org.web3j.protocol.websocket.WebSocketClient;
+import org.web3j.protocol.websocket.WebSocketService;
+import org.web3j.protocol.websocket.events.NewHead;
+import org.web3j.protocol.websocket.events.NewHeadsNotification;
+import org.web3j.protocol.websocket.events.NotificationParams;
 
 public class EthereumBlockWork extends EthereumWork {
 
@@ -50,14 +64,65 @@ public class EthereumBlockWork extends EthereumWork {
     public void run() {
         LOGGER.debug("run");
         try {
-            _run();
+            String nodeLocation = this.getNodeLocation();
+            if (nodeLocation.startsWith("ws:")) {
+                _runWebSocket(nodeLocation);
+            } else {
+                _run(nodeLocation);
+            }
         } catch (Exception ex) {
             LOGGER.error("error: " + ex.getMessage(), ex);
         }
     }
 
-    public void _run() throws Exception {
-        Web3j web3j = Web3jFactory.createWeb3j(this.getNodeLocation());
+    public void _runWebSocket(String nodeLocation) throws Exception {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Origin", "http://localhost");
+        WebSocketClient webSocketClient = new WebSocketClient(new URI(nodeLocation), headers);
+        WebSocketService webSocketService = new WebSocketService(webSocketClient, false);
+        webSocketService.connect();
+        Request<?, EthSubscribe> subscribeRequest = new Request<>(
+                "eth_subscribe",
+                Arrays.asList("newHeads", Collections.emptyMap()),
+                webSocketService,
+                EthSubscribe.class);
+
+        Flowable<NewHeadsNotification> events = webSocketService.subscribe(
+                subscribeRequest,
+                "eth_unsubscribe",
+                NewHeadsNotification.class
+        );
+
+        Disposable disposable = events.subscribe(event -> {
+            Date timestamp = new Date();
+            NotificationParams<NewHead> params = event.getParams();
+            NewHead newHead = params.getResult();
+            String blockHash = newHead.getHash();
+            for (EthereumActivationSpec ethereumActivationSpec : this.getEthereumActivationSpecs()) {
+                Boolean deliverBlock = ethereumActivationSpec.isDeliverBlock();
+                if (null == deliverBlock) {
+                    continue;
+                }
+                if (!deliverBlock) {
+                    continue;
+                }
+                // TODO: omitSyncing semantics
+                EthereumMessageListener ethereumMessageListener
+                        = ethereumActivationSpec.getEthereumMessageListener();
+                MessageEndpoint messageEndpoint = (MessageEndpoint) ethereumMessageListener;
+                messageEndpoint.beforeDelivery(BLOCK_METHOD);
+                try {
+                    ethereumMessageListener.block(blockHash, timestamp);
+                } catch (Exception e) {
+                    LOGGER.error("error invoking block: " + e.getMessage(), e);
+                }
+                messageEndpoint.afterDelivery();
+            }
+        });
+    }
+
+    public void _run(String nodeLocation) throws Exception {
+        Web3j web3j = Web3jFactory.createWeb3j(nodeLocation);
         BigInteger filterId = web3j.ethNewBlockFilter().send().getFilterId();
         while (!this.isShutdown()) {
             List<EthLog.LogResult> logResultList
--- ethereum-ra/src/main/java/be/e_contract/ethereum/ra/EthereumPendingTransactionWork.java
+++ ethereum-ra/src/main/java/be/e_contract/ethereum/ra/EthereumPendingTransactionWork.java
@@ -18,15 +18,28 @@
 package be.e_contract.ethereum.ra;
 
 import be.e_contract.ethereum.ra.api.EthereumMessageListener;
+import io.reactivex.Flowable;
+import io.reactivex.disposables.Disposable;
 import java.lang.reflect.Method;
 import java.math.BigInteger;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.resource.spi.endpoint.MessageEndpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.Request;
 import org.web3j.protocol.core.methods.response.EthLog;
+import org.web3j.protocol.core.methods.response.EthSubscribe;
+import org.web3j.protocol.websocket.WebSocketClient;
+import org.web3j.protocol.websocket.WebSocketService;
+import org.web3j.protocol.websocket.events.NotificationParams;
+import org.web3j.protocol.websocket.events.PendingTransactionNotification;
 
 public class EthereumPendingTransactionWork extends EthereumWork {
 
@@ -48,16 +61,65 @@ public class EthereumPendingTransactionWork extends EthereumWork {
 
     @Override
     public void run() {
-        LOGGER.debug("run");
         try {
-            _run();
+            String nodeLocation = this.getNodeLocation();
+            if (nodeLocation.startsWith("ws:")) {
+                _runWebSocket(nodeLocation);
+            } else {
+                _run(nodeLocation);
+            }
         } catch (Exception ex) {
             LOGGER.error("error: " + ex.getMessage(), ex);
         }
     }
 
-    public void _run() throws Exception {
-        Web3j web3j = Web3jFactory.createWeb3j(this.getNodeLocation());
+    public void _runWebSocket(String nodeLocation) throws Exception {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Origin", "http://localhost");
+        WebSocketClient webSocketClient = new WebSocketClient(new URI(nodeLocation), headers);
+        WebSocketService webSocketService = new WebSocketService(webSocketClient, false);
+        webSocketService.connect();
+        Request<?, EthSubscribe> subscribeRequest = new Request<>(
+                "eth_subscribe",
+                Arrays.asList("newPendingTransactions", Collections.emptyMap()),
+                webSocketService,
+                EthSubscribe.class);
+
+        Flowable<PendingTransactionNotification> events = webSocketService.subscribe(
+                subscribeRequest,
+                "eth_unsubscribe",
+                PendingTransactionNotification.class
+        );
+
+        Disposable disposable = events.subscribe(event -> {
+            Date timestamp = new Date();
+            NotificationParams<String> params = event.getParams();
+            String transactionHash = params.getResult();
+            for (EthereumActivationSpec ethereumActivationSpec : this.getEthereumActivationSpecs()) {
+                Boolean deliverPending = ethereumActivationSpec.isDeliverPending();
+                if (null == deliverPending) {
+                    continue;
+                }
+                if (!deliverPending) {
+                    continue;
+                }
+                // TODO: omitSyncing semantics
+                EthereumMessageListener ethereumMessageListener
+                        = ethereumActivationSpec.getEthereumMessageListener();
+                MessageEndpoint messageEndpoint = (MessageEndpoint) ethereumMessageListener;
+                messageEndpoint.beforeDelivery(PENDING_TRANSACTION_METHOD);
+                try {
+                    ethereumMessageListener.pendingTransaction(transactionHash, timestamp);
+                } catch (Exception e) {
+                    LOGGER.error("error invoking block: " + e.getMessage(), e);
+                }
+                messageEndpoint.afterDelivery();
+            }
+        });
+    }
+
+    public void _run(String nodeLocation) throws Exception {
+        Web3j web3j = Web3jFactory.createWeb3j(nodeLocation);
         BigInteger filterId = web3j.ethNewPendingTransactionFilter().send().getFilterId();
         while (!this.isShutdown()) {
             List<EthLog.LogResult> logResultList = web3j.ethGetFilterChanges(filterId).send().getResult();
