1071df315ceed98a477ee925d939305190cb5461
Frank Cornelis <info@e-contract.be>
2021-02-02 09:30:57 +0100

    additional testing

--- src/main/scripts/besu-dev.sh
+++ src/main/scripts/besu-dev.sh
@@ -2,11 +2,12 @@
 
 if [ $# == 0 ]
 then
-	echo "Usage: $0 <address>"
+	echo "Usage: $0 <your ethereum address on which will be mined>"
 	exit 1
 fi
 
 ADDRESS=$1
+echo "Miner address: $ADDRESS"
 
-BESU_HOME=$HOME/besu-20.10.1
-$BESU_HOME/bin/besu --network=dev --miner-enabled --miner-coinbase=$ADDRESS --rpc-http-cors-origins="all" --host-allowlist="*" --rpc-ws-enabled --rpc-http-enabled
+BESU_HOME=$HOME/besu-20.10.4
+$BESU_HOME/bin/besu --network=dev --miner-enabled --miner-coinbase="$ADDRESS" --rpc-http-cors-origins="all" --host-allowlist="*" --rpc-ws-enabled --rpc-http-enabled
